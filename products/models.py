075dc6fa36958166006af1dc0dda916fb6d80b20
Mustafa  Abdelmgeed <67156516+MustafaAhmed-star@users.noreply.github.com>
2023-12-10 22:11:36 +0200

    database
    add major models without fields

--- products/models.py
+++ products/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    pass
+
+
+class ProductImages(models.Model):
+    pass
+
+
+class Brand(models.Model):
+    pass
+    
+class Review(models.Model):
+    pass
