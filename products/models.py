262467bc0a908cdd798b9c640012eb0e3d460eef
Mustafa  Abdelmgeed <67156516+MustafaAhmed-star@users.noreply.github.com>
2023-12-12 22:37:13 +0200

    add slug

--- products/models.py
+++ products/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from  django.contrib.auth.models import User
 from taggit.managers import TaggableManager
+from django.utils.text import slugify
 FLAG_TYPES ={
     ('New','New'),
     ('Sale','Sale'),
@@ -18,10 +19,15 @@ class Product(models.Model):
     tags = TaggableManager()
     brand  = models.ForeignKey('Brand',related_name='product_brand' ,on_delete=models.SET_NULL,null = True)
     create_at = models.DateTimeField(auto_now=True)
-   
+    slug = models.SlugField(blank=True,null=True)
+    
     
     def __str__(self):
         self.name
+        
+    def save(self,*args,**kwargs):
+        self.slug = slugify(self.name)
+        super(Product, self).save(*args,**kwargs)
 class ProductImages(models.Model):
     product = models.ForeignKey(Product,related_name='product_image' ,on_delete=models.CASCADE)
     image = models.ImageField(upload_to='productImages')
