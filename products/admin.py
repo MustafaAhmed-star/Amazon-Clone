ecf7e212e34fb434b73f767ab58834faf00482a6
Mustafa  Abdelmgeed <67156516+MustafaAhmed-star@users.noreply.github.com>
2023-12-12 23:09:16 +0200

    merge productsImages to Product model admin to add it one time

--- products/admin.py
+++ products/admin.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from .models import *
 
 
-admin.site.register(Product)
-admin.site.register(ProductImages)
+class ProductImagesInline(admin.TabularInline):
+    model = ProductImages
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductImagesInline]
+admin.site.register(Product,ProductAdmin)
 admin.site.register(Brand)
 admin.site.register(Review)
