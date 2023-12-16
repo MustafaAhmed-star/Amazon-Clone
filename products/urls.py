8ff1c3eb67981b42240de89ad72775b2aebb1e59
Mustafa  Abdelmgeed <67156516+MustafaAhmed-star@users.noreply.github.com>
2023-12-16 22:31:15 +0200

    Add List&Detail view and it's patterns

---
+++ products/urls.py
@@ -0,0 +1,10 @@
+from django.urls import  path
+from . import views
+
+
+
+urlpatterns = [
+    path('',views.ProductList.as_view()),
+    path('<int:pk>/',views.ProductList.as_view()),
+    
+]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+from . import models
 
-# Create your views here.
+class ProductList(generic.ListView):
+    model = models.Product
+    
+class ProductDetail(generic.DetailView):
+    model = models.Product
