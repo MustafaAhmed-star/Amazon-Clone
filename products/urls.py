30bd6199296100c406d1fa22e719f310694cfd88
Mustafa  Abdelmgeed <67156516+MustafaAhmed-star@users.noreply.github.com>
2023-12-17 14:35:48 +0200

    add static files and templates

--- products/urls.py
+++ products/urls.py
@@ -6,6 +6,6 @@ from . import views
 urlpatterns = [
     path('',views.ProductList.as_view()),
     # path('<int:pk>/',views.ProductList.as_view()),
-    path('<slug:slug>/',views.ProductList.as_view()),
+    path('<slug:slug>/',views.ProductDetail.as_view()),
     
 ]
