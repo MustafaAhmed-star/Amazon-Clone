a76df5e1d814ea7c5d0d55acdeeec6138a6a8990
Mustafa  Abdelmgeed <67156516+MustafaAhmed-star@users.noreply.github.com>
2023-12-20 23:44:19 +0200

    show review-product logic in view

--- products/views.py
+++ products/views.py
@@ -7,3 +7,8 @@ class ProductList(generic.ListView):
     
 class ProductDetail(generic.DetailView):
     model = models.Product
+    
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        context["reviews"] = models.Review.objects.filter(product =self.get_object())
+        return context        
