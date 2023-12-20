46a7affdeac57335c566e6d832531b05b5ef54b3
Mustafa  Abdelmgeed <67156516+MustafaAhmed-star@users.noreply.github.com>
2023-12-20 23:45:53 +0200

    refactoring

--- products/views.py
+++ products/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 from django.views import generic
-from . import models
+from .models import Product,Review,ProductImages,Brand 
 
 class ProductList(generic.ListView):
-    model = models.Product
+    model = Product
     
 class ProductDetail(generic.DetailView):
-    model = models.Product
+    model = Product
     
     def get_context_data(self,**kwargs):
         context = super().get_context_data(**kwargs)
-        context["reviews"] = models.Review.objects.filter(product =self.get_object())
+        context["reviews"] =Review.objects.filter(product =self.get_object())
         return context        
