b76f1923ec4db01f7d0fd29c1fa73cbbb049e56a
Mustafa  Abdelmgeed <67156516+MustafaAhmed-star@users.noreply.github.com>
2023-12-21 15:02:57 +0200

    add brand detial and products in the same page

--- products/views.py
+++ products/views.py
@@ -19,5 +19,18 @@ class ProductDetail(generic.DetailView):
 class BrandList(generic.ListView):
     model = Brand
     
-class BrandDetail(generic.DetailView):
-    model = Brand
+class BrandDetail(generic.ListView):
+    model = Product
+    template_name = 'products/brand_detail.html '
+    def get_queryset(self):
+        brand  = Brand.objects.get(slug = self.kwargs['slug'])
+        queryset = super().get_queryset().filter(brand=brand)
+        return queryset
+        
+    def get_context_data(self, **kwargs) :
+        context = super().get_context_data(**kwargs)
+        context["brand"] = Brand.objects.get(slug = self.kwargs['slug'])
+        return context
+    
+        
+    
