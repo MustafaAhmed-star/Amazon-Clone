0b9dfad7aeb4707c0c604ef97da8dbbbf51055ae
Mustafa  Abdelmgeed <67156516+MustafaAhmed-star@users.noreply.github.com>
2023-12-24 22:53:36 +0200

    add: ordering to product List

--- products/api.py
+++ products/api.py
@@ -12,10 +12,11 @@ class ProductListApi(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = serializers.ProductListSerializers
     pagination_class = ProductPaginator
-    filter_backends = [DjangoFilterBackend,filters.SearchFilter] 
+    filter_backends = [DjangoFilterBackend,filters.SearchFilter,filters.OrderingFilter] 
     filterset_fields = ['brand', 'flag']
     search_fields = ['name','subtitle','description']
-
+    ordering_fields = ['price', ]
+    ordering = ['price'] # if i have more one fields use ordering to make a default
     
 class ProductDetailApi(generics.RetrieveAPIView):
     queryset = Product 
