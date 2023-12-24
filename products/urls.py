7489b0beedf8c6581e9d09675f7b02bbcc10615f
Mustafa  Abdelmgeed <67156516+MustafaAhmed-star@users.noreply.github.com>
2023-12-24 14:51:15 +0200

    fix: ordering patterns

--- products/urls.py
+++ products/urls.py
@@ -4,18 +4,19 @@ from .import api
 
 
 urlpatterns = [
+ #api
+    path('api/products',api.ProductListApi.as_view()),
+    path('api/products/<int:pk>',api.ProductDetailApi.as_view()),
+    path('api/brands',api.BrandListApi.as_view()),
+    path('api/brands/<int:pk>',api.BrandDetailApi.as_view()),
     
+    
+
+    ####
     path('brands/',views.BrandList.as_view()),
     path('brands/<slug:slug>/',views.BrandDetail.as_view()),
     path('',views.ProductList.as_view()),
     # path('<int:pk>/',views.ProductList.as_view()),
     path('<slug:slug>/',views.ProductDetail.as_view()),
-    
-    #api
-    path('api/list',api.ProductListApi.as_view()),
-    path('api/list/<int:pk>',api.ProductDetailApi.as_view()),
-    path('api/brands',api.BrandListApi.as_view()),
-    path('api/brands/<int:pk>',api.BrandDetailApi.as_view()),
-    
-    
-]
+ ]   
+   
