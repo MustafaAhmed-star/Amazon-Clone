e1bacfa414698b09eb4adfb50b25b7f6f8a829ab
Mustafa  Abdelmgeed <67156516+MustafaAhmed-star@users.noreply.github.com>
2023-12-25 14:08:38 +0200

    add: review_ count to List &detial product

--- products/serializers.py
+++ products/serializers.py
@@ -1,21 +1,25 @@
 from rest_framework import serializers
-from .models import Product , Brand
+from .models import Product , Brand ,Review
 
 
 
 class ProductListSerializers(serializers.ModelSerializer):
     brand = serializers.StringRelatedField() 
+    review_count = serializers.SerializerMethodField()
     class Meta:
         model = Product
         fields= '__all__'
-    
+    def get_review_count(self,object):
+        return object.review_product.all().count()
 class ProductDetailSerializers(serializers.ModelSerializer):
     brand = serializers.StringRelatedField() 
-   
+    review_count = serializers.SerializerMethodField()
     class Meta:
         model = Product
         fields= '__all__'
-    
+    def get_review_count(self,object):
+        return object.review_product.all().count()
+
 class BrandListSerializers(serializers.ModelSerializer):
     class Meta:
         model = Brand
