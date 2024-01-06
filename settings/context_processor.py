c960c8577132c07d7095999509980b16899aa0d1
Mustafa  Abdelmgeed <67156516+MustafaAhmed-star@users.noreply.github.com>
2024-01-07 01:22:23 +0200

    feat: cache to home_settings

--- settings/context_processor.py
+++ settings/context_processor.py
@@ -1,5 +1,17 @@
 from .models import Settings
+from django.core.cache import cache
+
+
 
 def get_settings(request):
-    data = Settings.objects.last()
+    
+    #check if data in cache
+    try:
+        data = cache.get('settings_data')
+        print("cach ")
+    except Exception :
+        print("New")
+        data = Settings.objects.last()
+        cache.set('settings_data',data,1)
+    
     return {'settings_data':data}
