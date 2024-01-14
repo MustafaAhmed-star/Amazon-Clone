eaeed151d779e1371db6d53ed3fa5532716686d0
Mustafa  Abdelmgeed <67156516+MustafaAhmed-star@users.noreply.github.com>
2024-01-14 10:19:20 +0200

    .

--- settings/views.py
+++ settings/views.py
@@ -4,7 +4,7 @@ from products.models import Product,Brand,Review
 def home(request):
     new_product = Product.objects.filter(flag='New')[:10]
     sale_product = Product.objects.filter(flag='Sale')[:10]
-    feature_product = Product.objects.filter(flag='Feature')[:10]
+    feature_product = Product.objects.filter(flag='Feature')[:6]
     brands = Brand.objects.annotate(product_count = Count('product_brand'))
     reviews = Review.objects.all()[:6]
     context = {
