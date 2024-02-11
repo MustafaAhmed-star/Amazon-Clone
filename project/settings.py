20db95d4e22a2a9e9a7e41c855aaec9e06557d01
Mustafa  Abdelmgeed <67156516+MustafaAhmed-star@users.noreply.github.com>
2024-02-11 07:09:56 +0200

    add : default auth class jwt

--- project/settings.py
+++ project/settings.py
@@ -168,8 +168,10 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 100,
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
+        # 'rest_framework.authentication.TokenAuthentication',
         # 'rest_framework.authentication.SessionAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+
     ]
 }
 
