3e467b0fca6d3c59e0c836d2f4efda41b6909229
Mustafa  Abdelmgeed <67156516+MustafaAhmed-star@users.noreply.github.com>
2024-03-13 14:22:32 +0200

    customize : admin to add order&orderItems in same page

--- orders/admin.py
+++ orders/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 from .models import Order, OrderItems,Coupon ,Cart,CartItems
 # Register your models here.
-admin.site.register(Order)
-admin.site.register(OrderItems)
+from products.models import Product
+class OrderItemInline(admin.TabularInline):
+    model = OrderItems
+
+
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [OrderItemInline]
+    # ... other admin configuration for Order
+
 admin.site.register(Coupon)
 admin.site.register(Cart)
 admin.site.register(CartItems)
